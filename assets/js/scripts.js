0d1cd4b3c33eb19ce9cd06817f47c9bfa5c2b393
aus-tn <74849260+aus-tn@users.noreply.github.com>
2025-01-22 12:18:47 -0500

    about, branding, case studies, process, and contact update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,17 +26,17 @@ $(document).ready(function () {
 
   //accordion
   var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 
 });
